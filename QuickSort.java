61f83d032844815b0ee77627c3be967e0d143c79
lourensterr <elourenzha19@gmail.com>
2020-01-15 13:14:26 +0700

    www

--- QuickSort.java
+++ QuickSort.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package quicksort;
-
 import java.util.Arrays;
 
-/**
- *
- * @author ester
- */
 public class QuickSort {
 
     
